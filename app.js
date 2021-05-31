187b9f64641b2ab98f2cf0c174f8fc4f225e5c52
Julia <jgurina.exceedteam@gmail.com>
2021-05-31 15:42:57 +0300

    create a route for deleting tasks; create a route for editing a task

--- app.js
+++ app.js
@@ -1,56 +1,63 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 const { Schema } = mongoose;
 
 const taskSheme = new Schema({
-    text: String,
-    isCheck: Boolean
+  text: String,
+  isCheck: Boolean,
 });
 
-const Task = mongoose.model('tasks', taskSheme);
+const Task = mongoose.model("tasks", taskSheme);
 
 app.use(cors());
 
-const url = "mongodb+srv://new_user:user987@cluster0.xtko5.mongodb.net/TestEducationDB?retryWrites=true&w=majority";
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+const url =
+  "mongodb+srv://new_user:user987@cluster0.xtko5.mongodb.net/TestEducationDB?retryWrites=true&w=majority";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 
-app.get ('/allTasks', (req, res) => {
-   Task.find().then(result => {
-       res.send({data: result});
-   }); 
+app.get("/allTasks", (req, res) => {
+  Task.find().then((result) => {
+    res.send({ data: result });
+  });
 });
 
-app.post ('/createTask', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(result => {
-        res.send('Task created');
-    })
+app.post("/createTask", (req, res) => {
+  const task = new Task(req.body);
+  task.save().then((result) => {
+    res.send("Task created");
+  });
 });
 
-// app.put ('/createTask', (req, res) => {
-//     const task = new Task(req.body);
-//     task.save().then(result => {
-//         res.send('Task created');
-//     })
-// });
+app.patch("/updateTask", (req, res) => {
+  const { _id } = req.body;
+  Task.updateOne({ _id }, req.body).then((result) => {
+    Task.find().then((result) => {
+      res.send({ data: result });
+    });
+  });
+});
 
-// // app.delete ('/deleteTask', (req, res) => {
-// //     const task = new Task(req.body);
-// //     task.save().then(result => {
-// //         res.send({data:tasks});
-// //     })
-// // });
+app.delete("/deleteTask", (req, res) => {
+  Task.deleteOne(req.body).then((result) => {
+    Task.find().then((result) => {
+      res.send({ data: result });
+    });
+  });
+});
 
-// app.delete ('/deleteTasks', (req, res) => {
-//     result = [];
-//     res.send({data: result});
-// });
+app.delete("/deleteTasks", (req, res) => {
+  Task.deleteMany(req.body).then((result) => {
+    Task.find().then((result) => {
+      res.send({ data: result });
+    });
+  });
+});
 
 app.listen(4000, () => {
-    console.log('Example app listening on port 4000!');
-})
+  console.log("Example app listening on port 4000!");
+});
